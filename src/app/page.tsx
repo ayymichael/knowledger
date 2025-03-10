0e25442a24b39addfdbcb7972cac664084b872be
Mikhail Panferov <mikhail.panferov@cyberia-tech.com>
2025-03-10 15:44:52 +0300

    added sme rubbish for testing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import CreateCourseForm from "@/modules/courses-list/pub/CreateCourseForm";
 export default function Home() {
   return (
     <main className="font-manrope flex min-h-screen flex-col p-8">
+      <h1>Testing prisma</h1>
       <CreateCourseForm
         revalidatePagePath="/"
         className="max-w-[300px] mb-10"

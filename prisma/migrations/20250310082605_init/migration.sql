7aadcc8a431ec03240ead458366b6c837bd4119f
Mikhail Panferov <mikhail.panferov@cyberia-tech.com>
2025-03-10 11:26:25 +0300

    New migrations

---
+++ prisma/migrations/20250310082605_init/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "Subject" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+
+    CONSTRAINT "Subject_pkey" PRIMARY KEY ("id")
+);

874b3e86989d7926a26a716bd82764d446121518
Mikhail Panferov <mikhail.panferov@cyberia-tech.com>
2025-03-14 14:20:25 +0300

    feat: added base authentication

--- src/widgets/app-header/_ui/profile.tsx
+++ src/widgets/app-header/_ui/profile.tsx
@@ -12,9 +12,21 @@ import {
 import { LogOut, User } from "lucide-react";
 import { Button } from "@/shared/ui/button";
 import Link from "next/link";
-import { Avatar, AvatarFallback } from "@radix-ui/react-avatar";
+import { Avatar, AvatarFallback, AvatarImage } from "@radix-ui/react-avatar";
+import { useAppSession } from "@/entities/session/use-app-session";
+import { useSignOut } from "@/modules/auth/use-sign-out";
+import { Skeleton } from "@/shared/ui/skeleton";
+import { SignInButton } from "@/modules/auth/SignInButton";
 
 export function Profile() {
+  const session = useAppSession();
+  const { signOut, isPending: isLoadingSignOut } = useSignOut();
+
+  if (session.status === "loading")
+    return <Skeleton className="w-8 h-8 rounded-full" />;
+
+  if (session.status === "unauthenticated") return <SignInButton />;
+
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
@@ -22,28 +34,43 @@ export function Profile() {
           variant="ghost"
           className="p-px rounded-full self-center h-8 w-8"
         >
-          <Avatar className="w-8 h-8 p-1.5">
-            <AvatarFallback>AC</AvatarFallback>
-          </Avatar>
+          {session.data ? (
+            <Avatar className="w-8 h-8 p-1.5">
+              <AvatarImage src={session.data.user.image} />
+              <AvatarFallback>
+                {session.data.user.name
+                  ? session.data.user.name.slice(0, 2)
+                  : "00"}
+              </AvatarFallback>
+            </Avatar>
+          ) : null}
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent className="w-56 mr-2 ">
-        <DropdownMenuLabel>
-          <p>Profile</p>
-          <p className="text-xs text-muted-foreground overflow-hidden text-ellipsis">
-            Test
-          </p>
-        </DropdownMenuLabel>
+        {session.data ? (
+          <DropdownMenuLabel>
+            <p>{session.data.user.name}</p>
+            <p className="text-xs text-muted-foreground overflow-hidden text-ellipsis">
+              {session.data.user.email}
+            </p>
+          </DropdownMenuLabel>
+        ) : null}
         <DropdownMenuGroup></DropdownMenuGroup>
         <DropdownMenuSeparator />
         <DropdownMenuGroup>
-          <DropdownMenuItem asChild>
-            <Link href="/profile/1">
-              <User className="mr-2 h-4 w-4" />
-              <span>View Profile</span>
-            </Link>
-          </DropdownMenuItem>
-          <DropdownMenuItem>
+          {session.data ? (
+            <DropdownMenuItem asChild className="cursor-pointer select-none">
+              <Link href={`/profile/${session.data.user.id}`}>
+                <User className="mr-2 h-4 w-4" />
+                <span>Profile</span>
+              </Link>
+            </DropdownMenuItem>
+          ) : null}
+          <DropdownMenuItem
+            disabled={isLoadingSignOut}
+            onClick={() => signOut()}
+            className="cursor-pointer select-none"
+          >
             <LogOut className="mr-2 h-4 w-4" />
             <span>Logout</span>
           </DropdownMenuItem>
